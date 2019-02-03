eca02ae3f25a1f7d0f93942a67e89941c566e7cb
macfa <kombo6768@gmail.com>
2019-02-03 20:03:44 +0900

    test

--- index.js
+++ index.js
@@ -4,18 +4,5 @@ const fs = require('fs');
 const assert = require('assert').strict;
 
 let server = require('./server');
-let router = require('./router');
-let requestHandlers = require('./requestHandlers');
-
-var handle = {};
-
-// connect a function of associate Pathname
-handle['/'] = requestHandlers.start;
-handle['/start'] = requestHandlers.start;
-handle['/upload'] = requestHandlers.upload;
-
-
-
-server.start(router.route, handle);
 
 
--- server.js
+++ server.js
@@ -11,25 +11,3 @@ const url = require('url');
 // 		});
 // 	}
 // }
-
-function start(route, handle) {
-	function onRequest(req,res) {
-    var pathname = url.parse(req.url).pathname;
-		console.log("onRequest() " + pathname + " received.");
-		route(handle, pathname,res);
-		res.writeHead(200, {"Content-Type": "text/plain"});
-		res.write("Hello World");
-		res.end();
-	}
-
-	http.createServer(onRequest).listen(`${port}`);
-	console.log(`Server Started : ${hostname}:${port}`);
-	
-}
-
-function upload() {
-	console.log("Request handler 'upload' was called.");
-}
-
-exports.start = start;
-exports.upload = upload;
