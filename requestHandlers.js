d977b6b54b96bff8ff5770c52c9cf72348c6c13d
macfa <kombo6768@gmail.com>
2019-02-03 23:16:18 +0900

    read data in file and saved at variable

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,21 @@
-function start() {
+var exec = require("child_process").exec;
+var fs = require('fs');
+
+
+function start(res) {
   console.log("Request handler 'start' was called.");
+  var body = fs.readFileSync('./html/index.html', 'utf8');
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(body);
+	console.log(body);
+	res.end();
 }
 
-function upload() {
-  console.log("Request handler 'upload' was called.");
+function upload(res) {
+	console.log("Request handler 'upload' was called.");
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write("Hello Upload");
+	res.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -4,22 +4,11 @@ const port = 3000;
 const fs = require('fs');
 const url = require('url');
 
-// function checkListOfSites(req,res) {
-// 	if (res.url == '/') {
-// 		fs.readFile('./html/index.html', 'utf8', function() {
-// 			if (err) console.error(err);
-// 		});
-// 	}
-// }
-
 function start(route, handle) {
 	function onRequest(req,res) {
     var pathname = url.parse(req.url).pathname;
-		console.log("onRequest() " + pathname + " received.");
-		route(handle, pathname,res);
-		res.writeHead(200, {"Content-Type": "text/plain"});
-		res.write("Hello World");
-		res.end();
+		console.log("onRequest() " + pathname + " received");
+		route(handle, pathname, res);
 	}
 
 	http.createServer(onRequest).listen(`${port}`);
@@ -27,9 +16,4 @@ function start(route, handle) {
 	
 }
 
-function upload() {
-	console.log("Request handler 'upload' was called.");
-}
-
 exports.start = start;
-exports.upload = upload;
