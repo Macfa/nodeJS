acf50aa2b6ac1c22dd606be21411724c9c008e28
macfa <kombo6768@gmail.com>
2019-02-15 17:42:21 +0900

    import css package using npm

--- router.js
+++ router.js
@@ -4,9 +4,9 @@ function route(handle, pathname, res) {
     handle[pathname](res);
   } else {
     console.log("No request handler found for " + pathname);
-		response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("404 Not found");
-    response.end();
+		res.writeHead(404, {"Content-Type": "text/plain"});
+    res.write("404 Not found");
+    res.end();
   }
 }
 
